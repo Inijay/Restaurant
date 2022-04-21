051585f03d227612a24ef84088cd20191db8b16d
Inioluwa Jacob <51286535+Inijay@users.noreply.github.com>
2022-04-21 23:44:22 +0100

    New Update

---
+++ coffee/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit src/App.js and save to reload.
+        </p>
+      </header>
+      <body>
+        <h1>
+          Welcome
+        </h1> 
+        <hr/>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa in, ducimus laboriosam non dolorem dignissimos voluptatum beatae unde pariatur, quod voluptates, quos deleniti fugiat et placeat qui obcaecati excepturi iste!
+        </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ coffee/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navibar'>
+        <div className='logo'>
+            /* <img src={logo} className="App-logo" alt="logo" />
+             <img src='' alt='' width={} height/> */
+        </div>
+        <div className='navi-link'>
+            <li> Home </li>
+            <li> Product </li>
+            <li> About </li>
+            <li> Contact </li>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
